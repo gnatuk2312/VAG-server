fe656b18e7a08661539add7747b5013ce163b0a7
Vladyslav Babiak <vbabiak@atlassian.com>
2022-09-04 22:57:10 +0200

    VAG-61: Add tests

---
+++ tests/middleware/wrapper.test.js
@@ -0,0 +1,17 @@
+describe('test wrapper', () => {
+	it("Wrapper should call controller with req and res, if it gets all arguments and controller didn't call next", () => {
+
+	});
+
+	it("Wrapper should call controller with req and res, then call next with an error, if it gets all arguments and controller threw error", () => {
+
+	});
+
+	it("Wrapper should return Error, if it don't gets controller", () => {
+
+	});
+
+	it("Wrapper should return Error, if it don't gets req, res, next", () => {
+
+	});
+})
