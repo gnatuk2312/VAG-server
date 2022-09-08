a7283f3781d58d19c8501f7f16b8589ea8bc6930
gnatuk2312 <hnat.liashenko@bluepes.com>
2022-09-08 14:24:20 +0300

    VAG-66: modify models

--- src/models/clients.js
+++ src/models/clients.js
@@ -1,28 +1,32 @@
 const { Schema, model } = require('mongoose');
 
-const clientSchema = Schema({
-	name: {
-		type: String,
+const clientSchema = Schema(
+	{
+		name: {
+			type: String,
+		},
+		phone: {
+			type: String,
+		},
+		carBrand: {
+			type: String,
+		},
+		carModel: {
+			type: String,
+		},
+		licensePlate: {
+			type: String,
+		},
+		email: {
+			type: String,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
 	},
-	phone: {
-		type: String,
-	},
-	carBrand: {
-		type: String,
-	},
-	carModel: {
-		type: String,
-	},
-	licensePlate: {
-		type: String,
-	},
-	email: {
-		type: String,
-	},
-	favorite: {
-		type: Boolean,
-	},
-});
+	{ versionKey: false, timestamps: true },
+);
 
 const Client = model('client', clientSchema);
 
--- src/models/visits.js
+++ src/models/visits.js
@@ -1,26 +1,29 @@
 const { Schema, model } = require('mongoose');
 
-const visitSchema = Schema({
-	clientId: {
-		type: Schema.Types.ObjectId,
-		ref: 'Client',
+const visitSchema = Schema(
+	{
+		clientId: {
+			type: Schema.Types.ObjectId,
+			ref: 'client',
+		},
+		date: {
+			type: Date,
+		},
+		type: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		price: {
+			type: Number,
+		},
+		status: {
+			type: String,
+		},
 	},
-	date: {
-		type: Date,
-	},
-	type: {
-		type: String,
-	},
-	description: {
-		type: String,
-	},
-	price: {
-		type: Number,
-	},
-	status: {
-		type: String,
-	},
-});
+	{ versionKey: false, timestamps: true },
+);
 
 const Visit = model('visit', visitSchema);
 
